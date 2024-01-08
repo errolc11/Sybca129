fd12a32dfb672ebf873d935455f520c30d27e51b
Errol Caeiro <errolcaeiro07@gmail.com>
2024-01-08 09:47:17 +0530

    First github commit

---
+++ sybca129Project/src/module-info.java
@@ -0,0 +1,2 @@
+module sybca129Project {
+}
---
+++ sybca129Project/src/sybca129Project/ClassGithub.java
@@ -0,0 +1,12 @@
+package sybca129Project;
+
+public class ClassGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello world");
+		
+	}
+
+}
