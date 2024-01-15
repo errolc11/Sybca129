7846b9543e68f81e829afa96168444eef89ceaaa
errolc11 <155938056+errolc11@users.noreply.github.com>
2024-01-15 10:39:47 +0530

    Update add2nos.java

--- sybca129Project/src/sybca129Project/add2nos.java
+++ sybca129Project/src/sybca129Project/add2nos.java
@@ -5,11 +5,13 @@ public class add2nos {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int num1=2;
-		int num2=12;
-		int add;
-		add=num1+num2;
-		System.out.println("The sum is "+add);
+		int num1=5;
+		int num2=7;
+	        int sum = addNumbers(num1,num2);
+		System.out.println("Sum:"+sum);
+	}
+	public static int addNum(int a , int b){
+		return a+b;
 
 	}
 
