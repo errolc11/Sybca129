5b9382026a0039e85f03b41dce9b3ce7c18224b7
Errol Caeiro <errolcaeiro07@gmail.com>
2024-01-15 09:59:56 +0530

    Commit Message

---
+++ sybca129Project/src/sybca129Project/add2nos.java
@@ -0,0 +1,16 @@
+package sybca129Project;
+
+public class add2nos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num1=2;
+		int num2=12;
+		int add;
+		add=num1+num2;
+		System.out.println("The sum is "+add);
+
+	}
+
+}
